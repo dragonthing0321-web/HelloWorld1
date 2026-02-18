bd2d175864931b7a8824d33faf9f6a7e8a5dbac6
Casanova <dragonthing0321@gmail.com>
2026-02-18 09:19:59 -0600

    Did some cool stuff
    i coded a thing

--- HelloWorld1/Program.cs
+++ HelloWorld1/Program.cs
@@ -14,14 +14,14 @@ namespace HelloWorld1
             System.Console.WriteLine("Hello User! Input your first name so I may address you formally please!");
             string Firstname = System.Console.ReadLine();
             //Asks the user for their surname
-            System.Console.WriteLine("Hello " + Firstname + "! And your last name?");
+            System.Console.WriteLine("Hello " + firstName + "! And your last name?");
             string Lastname = System.Console.ReadLine();
             //This let's the user confirm whether or not this is their name or not, and if not, it allows them to try again.
-            System.Console.WriteLine("So your name is " + Firstname + " " + Lastname + "? Is that correct? (Y/N)");
+            System.Console.WriteLine("So your name is " + firstName + " " + lastName + "? Is that correct? (Y/N)");
             string Answer = System.Console.ReadLine();
                 if (Answer == "Y" || Answer == "y")
             {
-                System.Console.WriteLine("Perfect! You have a wonderful name " + Firstname + " " + Lastname + "!");
+                System.Console.WriteLine("Perfect! You have a wonderful name " + firstName + " " + lastName + "!");
             }
             else if (Answer == "N" || Answer == "n")
             {
